fd8055a82e2f238b7cde7bf86140ec166a2dd4a1
Pietrodon <donvitopietro00@gmail.com>
2022-04-08 17:37:04 +0200

    creazione istanze per i film e stampa a schermo valori

--- index.php
+++ index.php
@@ -4,17 +4,19 @@ class Movie {
     public $name;
     public $year;
     public $actor;
+    public $time;
 
-    function __construct( string $name, int $year, string $actor){
+    function __construct( string $name, int $year, string $actor, int $time){
         $this->name =$name;
         $this->year =$year;
         $this->actor =$actor;
-
+        $this->time=$time;
+            
     }
 }
 
-$bond = new Movie ("007",2020,"Daniel Craig");
-
+$bond = new Movie ("007",2020,"Daniel Craig",163);
+$morbius = new Movie ("Morbius",2022,"Jared Leto",108)
 ?>
 
 
@@ -29,14 +31,33 @@ $bond = new Movie ("007",2020,"Daniel Craig");
 </head>
 <body>
     <h1>
-        Il titolo del film è  <?php echo $bond->name ?>
+        Il titolo del film è  <?php echo $bond->name ?>.
     </h1>
     <ul>
         <li>
-            L'anno di uscita del film è <?php echo $bond->year ?>
+            L'anno di uscita del film è <?php echo $bond->year ?>.
         </li>
         <li>
-            L'attore protagonista è  <?php echo $bond->actor ?>
+            L'attore protagonista è  <?php echo $bond->actor ?>.
+        </li>
+        <li>
+            La durata del film è  <?php echo $bond->time ?> minuti.
+        </li>
+    </ul>
+
+
+    <h1>
+        Il titolo del film è  <?php echo $morbius->name ?>.
+    </h1>
+    <ul>
+        <li>
+            L'anno di uscita del film è <?php echo $morbius->year ?>.
+        </li>
+        <li>
+            L'attore protagonista è  <?php echo $morbius->actor ?>.
+        </li>
+        <li>
+            La durata del film è  <?php echo $morbius->time ?> minuti.
         </li>
     </ul>
 </body>
