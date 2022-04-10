2563dcf36515afbaf4edaa6149a1197a5d31b71f
Pietrodon <donvitopietro00@gmail.com>
2022-04-11 01:40:03 +0200

    metodo con errore

--- index.php
+++ index.php
@@ -4,19 +4,24 @@ class Movie {
     public $name;
     public $year;
     public $actor;
-    public $time;
+    public $incasso;
+    
+    
 
-    function __construct( string $name, int $year, string $actor, int $time){
+    function __construct( string $name, int $year, string $actor){
         $this->name =$name;
         $this->year =$year;
         $this->actor =$actor;
-        $this->time=$time;
-            
+    
+        
     }
+    public function numeroRandom() {
+         return $this->rand(min,max);
+    }  
 }
 
-$bond = new Movie ("007",2020,"Daniel Craig",163);
-$morbius = new Movie ("Morbius",2022,"Jared Leto",108)
+$bond = new Movie ("007",2020,"Daniel Craig");
+$morbius = new Movie ("Morbius",2022,"Jared Leto");
 ?>
 
 
@@ -27,7 +32,7 @@ $morbius = new Movie ("Morbius",2022,"Jared Leto",108)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Movie</title>
 </head>
 <body>
     <h1>
@@ -41,24 +46,11 @@ $morbius = new Movie ("Morbius",2022,"Jared Leto",108)
             L'attore protagonista è  <?php echo $bond->actor ?>.
         </li>
         <li>
-            La durata del film è  <?php echo $bond->time ?> minuti.
+            L'incasso al day one è stato di <?php echo $incasso->numeroRandom(1,100) ?> milioni di dollari.
         </li>
     </ul>
 
 
-    <h1>
-        Il titolo del film è  <?php echo $morbius->name ?>.
-    </h1>
-    <ul>
-        <li>
-            L'anno di uscita del film è <?php echo $morbius->year ?>.
-        </li>
-        <li>
-            L'attore protagonista è  <?php echo $morbius->actor ?>.
-        </li>
-        <li>
-            La durata del film è  <?php echo $morbius->time ?> minuti.
-        </li>
-    </ul>
+
 </body>
 </html>
